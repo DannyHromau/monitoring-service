3222f45c52933b632e5542fdc167c0e5faeedbae
Hromau <dgromov770@gmail.com>
2024-02-14 13:50:51 +0300

    feat: add javadoc

--- src/main/java/com/dannyhromau/monitoring/meter/facade/AuthFacade.java
+++ src/main/java/com/dannyhromau/monitoring/meter/facade/AuthFacade.java
@@ -7,7 +7,31 @@ import com.dannyhromau.monitoring.meter.exception.UnAuthorizedException;
 
 import java.sql.SQLException;
 
+/**
+ * @author Daniil Hromau
+ *
+ * Facade is using as converting layer between controller and service to separate the logic of working with
+ * dto and entity and mapper's injection
+ *
+ */
 public interface AuthFacade<T> {
+    /**
+     *
+     * @param authDto dto with credentials
+     * @return authenticated item
+     * @throws DuplicateDataException when data exists in DB
+     * @throws SQLException if something goes wrong when it's working with DB
+     * @throws InvalidDataException when invalid data references by client
+     * @throws EntityNotFoundException when entity doesn't exist in DB
+     */
     T register(T authDto) throws DuplicateDataException, SQLException, InvalidDataException, EntityNotFoundException;
+
+    /**
+     *
+     * @param authDto dto with credentials
+     * @return authenticated item
+     * @throws SQLException if something goes wrong when it's working with DB
+     * @throws UnAuthorizedException when referenced credentials are invalid
+     */
     T authorize(T authDto) throws SQLException, UnAuthorizedException;
 }
--- src/main/java/com/dannyhromau/monitoring/meter/facade/MeterReadingFacade.java
+++ src/main/java/com/dannyhromau/monitoring/meter/facade/MeterReadingFacade.java
@@ -10,6 +10,13 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.List;
 
+/**
+ * @author Daniil Hromau
+ *
+ * Facade is using as converting layer between controller and service to separate the logic of working with
+ * dto and entity and mapper's injection
+ *
+ */
 public interface MeterReadingFacade {
 
     MeterReadingDto add(MeterReadingDto meterReadingDto)
@@ -18,9 +25,38 @@ public interface MeterReadingFacade {
     List<MeterReadingDto> getByUserId(long userId) throws SQLException;
     List<MeterReadingDto> getByUserIdAndMeterType(long userId, long mrTypeId) throws SQLException;
     MeterReadingDto getById(long id) throws EntityNotFoundException, SQLException;
+
+    /**
+     * getting actual meter reading
+     * @param userId
+     * @param mrTypeId meter type id
+     * @return last posted meter reading
+     * @throws EntityNotFoundException when data don't exist
+     * @throws SQLException
+     */
     MeterReadingDto getActualMeterReading(long userId, long mrTypeId) throws EntityNotFoundException, SQLException;
+
+    /**
+     * getting meter reading by date
+     * @param userId
+     * @param date LocalDate standard form
+     * @param mrTypeId meter type id
+     * @return meter reading
+     * @throws EntityNotFoundException when data don't exist
+     * @throws SQLException
+     */
     MeterReadingDto getMeterReadingByDateAndMeterType(long userId, LocalDate date, long mrTypeId)
             throws EntityNotFoundException, SQLException;
+
+    /**
+     * getting meter reading by year and month
+     * @param userId
+     * @param yearMonth format
+     * @param mrTypeId
+     * @return
+     * @throws EntityNotFoundException
+     * @throws SQLException
+     */
     MeterReadingDto getMeterReadingByMonthAndMeterType(long userId, YearMonth yearMonth, long mrTypeId)
             throws EntityNotFoundException, SQLException;
 }
